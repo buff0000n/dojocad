8ad56225b014d62225ab1f9e709a8b1070c7d718
Buff00n <git@manwithtoomanyhats.com>
2022-07-05 17:43:54 -0400

    Added a visual marker for the piece of glass on the Starlight Market that clips into the adjacent room

--- js/room-metadata.js
+++ js/room-metadata.js
@@ -252,6 +252,8 @@ var roomMetadata =
 		{"x": 0, "y": -32, "floor": 0, "outx": 0, "outy": -1},
 		{"x": -32, "y": -16, "floor": 0, "outx": -1, "outy": 0},
 		{"x": -32, "y": 16, "floor": 0, "outx": -1, "outy": 0}],
+	"floor_images": [
+		{"floor": 0, "image": "starlight-market", "marker_images": [{"image": "marker-starlight-market-glass", "x": 22.5, "y": -43, "rot": 0}]}],
 	"resources": [
 		{"resource": "Credits", "costs": [1000, 3000, 10000, 30000, 100000]},
 		{"resource": "Ferrite", "costs": [800, 2400, 8000, 24000, 80000]},
